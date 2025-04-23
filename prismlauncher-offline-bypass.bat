f678501cf55e3428d52a6e0d4936656fe92f294b
Creeper'sss <137390247+Creepersss-gh@users.noreply.github.com>
2025-04-23 20:42:05 +0200

    Create prismlauncher-offline-bypass.bat

---
+++ prismlauncher-offline-bypass.bat
@@ -0,0 +1,1 @@
+echo '{"accounts": [{"entitlement": {"canPlayMinecraft": true,"ownsMinecraft": true},"type": "MSA"}],"formatVersion": 3}' > ~/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/accounts.json
